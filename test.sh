260fad496869d57d2176b495049d27eaf718a8bd
stevepeak <steve@stevepeak.net>
2015-08-20 18:25:24 -0400

    wip

--- test.sh
+++ test.sh
@@ -2,12 +2,36 @@
 
 set -e
 
+function set_state() {
+    # set head of wip to pending
+    curl -X POST "https://api.github.com/repos/codecov/$0/statuses/$1" \
+         -d "{\"state\": \"$2\",\
+              \"target_url\": \"https://circleci.com/gh/codecov/testsuite/$CIRCLE_BUILD_NUM\",\
+              \"description\": \"$3\",\
+              \"context\": \"ci/testsuite\"}"
+}
+
+function get_head() {
+    return $(curl -sX GET "https://api.github.com/repos/codecov/$0/git/refs/wip" |\
+             python -c "import sys,json;print(json.loads(sys.stdin.read())['object']['sha'])")
+}
+
+# get head of wip branches
+codecovbash=$(get_head 'codecov-bash')
+codecovpython=$(get_head 'codecov-python')
+
+# set pending status for heads
+set_state "codecov-bash" "$codecovbash" "pending" "Pending..."
+set_state "codecov-python" "$codecovpython" "pending" "Pending..."
+
 git config --global user.email "hello@codecov.io"
 git config --global user.name "Codecov Test Bot"
 
 repos=('example-java' 'example-scala' 'example-xcode')
-urls=()
+total="${#repos[@]}"
+passed=0
 
+urls=()
 for repo in ${repos[*]}
 do
     git clone -b future git@github.com:codecov/$repo.git
@@ -29,14 +53,35 @@ do
     for url in ${urls[*]}
     do
         echo "Checking $url..."
-        state=$(curl -s "$url" | python -c "import sys,json;print(json.loads(sys.stdin.read())['state'])")
+        state=$(curl -sX GET "$url" | python -c "import sys,json;print(json.loads(sys.stdin.read())['state'])")
         echo -n "$state"
         if [ "$state" = "success" ];
         then
+            # no longer need to check
             url=${urls[@]/"$url"}
+            # record passed
+            passed=$(expr $passed + 1)
         elif [ "$state" != "pending" ];
         then
-            exit 1;
+            # no longer need to check
+            url=${urls[@]/"$url"}
         fi
     done
 done
+
+# submit states
+if [ "$passed" = "$total" ];
+then
+  state="success"
+else
+  state="failure"
+fi
+
+# set state status for heads
+set_state "codecov-bash" "$codecovbash" "$state" "$passed/$total successful"
+set_state "codecov-python" "$codecovpython" "$state" "$passed/$total successful"
+
+if [ "$state" != "success" ];
+then
+  exit 1;
+fi
