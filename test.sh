5e269e8051a6d16d86624945c034c5334467bf53
stevepeak <steve@stevepeak.net>
2015-08-20 17:59:35 -0400

    wait for builds

--- test.sh
+++ test.sh
@@ -5,14 +5,35 @@ set -e
 git config --global user.email "hello@codecov.io"
 git config --global user.name "Codecov Test Bot"
 
-for repo in 'example-java' 'example-scala' 'example-xcode'
+repos=('example-java' 'example-scala' 'example-xcode')
+urls=()
+
+for repo in ${repos[*]}
 do
     git clone -b future git@github.com:codecov/$repo.git
     cd "$repo"
     git commit --allow-empty -m "circle #$CIRCLE_BUILD_NUM"
+    # https://developer.github.com/v3/repos/statuses/#get-the-combined-status-for-a-specific-ref
+    urls+=("https://api.github.com/repos/codecov/$repo/commits/$(git rev-parse --HEAD)/status")
     git push origin future
     cd ../
 done
 
-# wait for all ci to complete
-# ...todo
+# wait for travis to pick up builds
+sleep 5
+
+while [ "${#urls[@]}" != "0" ]
+do
+    sleep 5
+    # collect build numbers
+    for url in ${urls[*]}
+    do
+        state=$(curl -s "$url" | python -c "import sys,json;print(json.loads(sys.stdin.read())['state'])")
+        if [ "$state" = "success" ];
+        then
+            url=${urls[@]/"$url"}
+        elif [ "$state" != "pending" ];
+            exit 1;
+        fi
+    done
+done
