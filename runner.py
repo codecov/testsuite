890a31e7c6cec5d6d0972acf5b58c756cd800968
Steve Peak <steve@stevepeak.net>
2015-10-02 09:02:31 +0200

    better logging [ci skip]

--- runner.py
+++ runner.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import time
+import logging
 import requests
 import subprocess
 from json import dumps
 
+# https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning
+logging.captureWarnings(True)
 
 headers = {"Authorization": "token "+os.getenv("GITHUB_TOKEN")}
 circleurl = "https://circleci.com/gh/codecov/testsuite/"+os.getenv("CIRCLE_BUILD_NUM")
@@ -35,19 +38,19 @@ def set_state(slug, commit, state):
 
 
 def get_head(slug, branch):
-    print("\033[92mGet head\033[0m")
+    print(slug + "\033[92mGet head\033[0m")
     res = curl('get', "https://api.github.com/repos/%s/git/refs/heads/%s" % (slug, branch), headers=headers)
     return res.json()['object']['sha']
 
 
 def get_tree(slug, commit):
-    print("\033[92mGet tree\033[0m")
+    print(slug + "\033[92mGet tree\033[0m")
     res = curl('get', "https://api.github.com/repos/%s/git/commits/%s" % (slug, commit), headers=headers)
     return res.json()['tree']['sha']
 
 
 def update_reference(slug, ref, commit):
-    print("\033[92mPatch reference\033[0m")
+    print(slug + "\033[92mPatch reference\033[0m")
     curl('patch', "https://api.github.com/repos/%s/git/refs/heads/%s" % (slug, ref), headers=headers,
          data=dumps(dict(sha=commit)))
     return True
@@ -56,7 +59,7 @@ def update_reference(slug, ref, commit):
 try:
     repos = ['codecov/example-java', 'codecov/example-scala', 'codecov/example-xcode', 'codecov/example-c',
              'codecov/example-lua', 'codecov/example-go', 'codecov/example-python', 'codecov/example-php',
-             'codecov/example-d', 'codecov/example-fortran']
+             'codecov/example-d', 'codecov/example-fortran', 'codecov/example-swift']
     total = len(repos)
 
     lang = os.getenv('TEST_LANG', 'bash')
@@ -65,6 +68,7 @@ try:
     cmd = os.getenv('TEST_CMD', None)
     if not cmd:
         if lang == 'python':
+            repos.remove('codecov/example-swift')  # bash only atm
             cmd = 'pip install --user git+https://github.com/%s.git@%s && codecov' % (slug, sha)
         elif lang == 'bash':
             repos.remove('codecov/example-c')  # python only
@@ -79,7 +83,7 @@ try:
         # https://developer.github.com/v3/git/commits/#create-a-commit
         head = get_head(_slug, 'future')
         tree = get_tree(_slug, head)
-        print("\033[92mPost commit\033[0m")
+        print(_slug + "\033[92mPost commit\033[0m")
         res = curl('post', "https://api.github.com/repos/%s/git/commits" % _slug,
                    headers=headers,
                    data=dumps(dict(message="Circle build #%s\n%s\n%s" % (os.getenv('CIRCLE_BUILD_NUM'), circleurl, cmd),
@@ -101,13 +105,13 @@ try:
         time.sleep(60)
         # collect build numbers
         for _slug, commit in commits.items():
-            print("Checking Travis %s at %s..." % (_slug, commit))
+            print(_slug + "Checking Travis %s at %s..." % (_slug, commit))
             res = curl('get', "https://api.github.com/repos/%s/commits/%s/status" % (_slug, commit), headers=headers)
             state = res.json()['state']
             print(state)
             assert state in ('success', 'pending')
             if state == 'success':
-                print("Checking %s at %s..." % (_slug, commit))
+                print(_slug + "Checking %s at %s..." % (_slug, commit))
                 future = curl('get', "https://codecov.io/api/gh/%s?ref=%s" % (_slug, commit))
                 if future.status_code == 404:
                     assert commit in future.json()['queue'], "%s at %.7s is not in Codecov upload queue" % (_slug, commit)
