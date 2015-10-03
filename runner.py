11ceb958695dc5d988c38ebfa32df07435e8f9f2
Steve Peak <steve@stevepeak.net>
2015-10-03 15:54:49 +0200

    fix [ci skip]

--- runner.py
+++ runner.py
@@ -5,6 +5,7 @@ import logging
 import requests
 import subprocess
 from json import dumps
+from difflib import unified_diff
 
 # https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning
 logging.captureWarnings(True)
@@ -13,14 +14,15 @@ headers = {"Authorization": "token "+os.getenv("GITHUB_TOKEN")}
 circleurl = "https://circleci.com/gh/codecov/testsuite/"+os.getenv("CIRCLE_BUILD_NUM")
 
 
-def curl(method, *args, **kwargs):
+def curl(method, reraise=True, *args, **kwargs):
     "wrapper to only print on errors"
     res = getattr(requests, method)(*args, **kwargs)
     try:
         res.raise_for_status()
     except:
         print(res.text)
-        raise
+        if reraise:
+            raise
     return res
 
 
@@ -28,13 +30,14 @@ def bash(cmd):
     return subprocess.check_output(cmd, shell=True).decode('utf-8')
 
 
-def set_state(slug, commit, state):
+def set_state(slug, commit, state, context):
     # set head of wip to pending
     return curl('post', "https://api.github.com/repos/%s/statuses/%s" % (slug, commit),
                 headers=headers,
                 data=dumps(dict(state=state,
+                                description=description,
                                 target_url=circleurl,
-                                context="codecov/examples")))
+                                context=context)))
 
 
 def get_head(slug, branch):
@@ -68,18 +71,19 @@ try:
     cmd = os.getenv('TEST_CMD', None)
     if not cmd:
         if lang == 'python':
-            repos.remove('codecov/example-swift')  # bash only atm
+            repos.remove('codecov/example-swift')  # bash only atm because https://travis-ci.org/codecov/example-xcode/builds/83448813
+            repos.remove('codecov/example-xcode')  # bash only atm because https://travis-ci.org/codecov/example-xcode/builds/83448813
             cmd = 'pip install --user git+https://github.com/%s.git@%s && codecov' % (slug, sha)
         elif lang == 'bash':
             repos.remove('codecov/example-c')  # python only
             cmd = 'bash <(curl -s https://raw.githubusercontent.com/%s/%s/codecov)' % (slug, sha)
 
-    # set pending status
-    set_state(slug, sha, "pending")
-
     # Make empty commit
     commits = {}
     for _slug in repos:
+        # set pending status
+        set_state(slug, sha, "pending", _slug)
+
         # https://developer.github.com/v3/git/commits/#create-a-commit
         head = get_head(_slug, 'future')
         tree = get_tree(_slug, head)
@@ -101,40 +105,64 @@ try:
     time.sleep(240)
 
     # Wait for CI Status
-    passed = 0
+    passed, total = 0, len(commits)
     while len(commits) > 0:
         print("Waiting 1 minutes...")
         time.sleep(60)
         # collect build numbers
         for _slug, commit in commits.items():
-            print(_slug + " Checking Travis %s at %s..." % (_slug, commit))
-            res = curl('get', "https://api.github.com/repos/%s/commits/%s/status" % (_slug, commit), headers=headers).json()
-            state = res['state']
-            print(_slug + ' State: ' + state + ' ' + res['statuses'][0]['target_url'])
-            assert state in ('success', 'pending')
-            if state == 'success':
-                print(_slug + " Checking %s at %s..." % (_slug, commit))
-                future = curl('get', "https://codecov.io/api/gh/%s?ref=%s" % (_slug, commit))
-                if future.status_code == 404:
-                    assert commit in future.json()['queue'], "%s at %.7s is not in Codecov upload queue" % (_slug, commit)
+            try:
+                res = curl('get', "https://api.github.com/repos/%s/commits/%s/status" % (_slug, commit), headers=headers).json()
+                state = res['state']
+                if state == 'pending':
                     continue
 
-                master = curl('get', "https://codecov.io/api/gh/%s?branch=master" % _slug)
+                print(_slug)
+                print('    \033[92mCI Status:\033[0m ' + state + ' @ ' + res['statuses'][0]['target_url'])
 
-                assert master.json()['report'] == future.json()['report'], "%s at %.7s reports do not match" % (_slug, commit)
+                # ASSERT status must be successful
+                assert state == 'success', "CI status %s" % state
+
+                # get future report
+                future = curl('get', "https://codecov.io/api/gh/%s?ref=%s" % (_slug, commit), reraise=False)
+                if future.status_code == 404:
+                    # ASSERT is queued for processing
+                    assert commit in future.json()['queue'], "%s at %.7s is not in Codecov upload queue" % (_slug, commit)
+                    # it is...try again later
+                    print("   In queue...")
+                    continue
+
+                assert future.status_code == 200, "Codecov returned %d" % future.status_code
+
+                future = future.json()['report']
+
+                # get master report to compare against
+                master = curl('get', "https://codecov.io/api/gh/%s?branch=master" % _slug).json()['report']
+                # reports must be 100% identical
+                if master == future:
+                    print("    Report passed!")
+                    set_state(slug, sha, 'success', _slug)
+                    passed += 1
+
+                else:
+                    diff = unified_diff(dumps(master, indent=2, sort_keys=True).split('\n'),
+                                        dumps(future, indent=2, sort_keys=True).split('\n'),
+                                        fromfile='master', tofile='future')
+                    # https://developer.github.com/v3/gists/#edit-a-gist
+                    res = curl('post', 'https://api.github.com/gists', headers=headers,
+                               data=dumps(dict(description=_slug, files={"diff.diff": {"content": "".join((diff.next(), diff.next(), diff.next(), "\n".join(diff)))}})))
+                    print("    Report Failed.")
+                    set_state(slug, sha, 'failed', _slug, res.json()['html_url'])
 
                 del commits[_slug]
-                passed = passed + 1
 
-    # submit states
-    status = 'success' if len(commits) == 0 else 'failure'
+            except Exception as e:
+                set_state(slug, sha, 'error', _slug, str(e))
+                print('    '+str(e))
+                del commits[_slug]
 
-    # set state status for heads
-    set_state(slug, sha, status)
-
-    sys.exit(status == 'failure')
+    sys.exit(passed < total)
 
 except Exception:
-    # set state status for heads
-    set_state(slug, sha, 'error')
+    [set_state(slug, sha, 'error', _slug, str(e)) for _slug in commits.keys()]
     raise
