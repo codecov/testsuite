ccd4c398b1ead0ce08641c6454c05a52c452d972
Steve Peak <steve@stevepeak.net>
2015-10-03 16:19:05 +0200

    wip [ci skip]

--- runner.py
+++ runner.py
@@ -3,7 +3,6 @@ import sys
 import time
 import logging
 import requests
-import subprocess
 from json import dumps
 from difflib import unified_diff
 
@@ -27,12 +26,9 @@ def curl(method, *args, **kwargs):
     return res
 
 
-def bash(cmd):
-    return subprocess.check_output(cmd, shell=True).decode('utf-8')
-
-
 def set_state(slug, commit, state, context, description=None, url=None):
     # set head of wip to pending
+    print("    \033[92mpost status\033[0m")
     return curl('post', "https://api.github.com/repos/%s/statuses/%s" % (slug, commit),
                 headers=headers,
                 data=dumps(dict(state=state,
@@ -42,19 +38,19 @@ def set_state(slug, commit, state, context, description=None, url=None):
 
 
 def get_head(slug, branch):
-    print(slug + " \033[92mGet head\033[0m")
+    print("    \033[92mget head\033[0m")
     res = curl('get', "https://api.github.com/repos/%s/git/refs/heads/%s" % (slug, branch), headers=headers)
     return res.json()['object']['sha']
 
 
 def get_tree(slug, commit):
-    print(slug + " \033[92mGet tree\033[0m")
+    print("    \033[92mget tree\033[0m")
     res = curl('get', "https://api.github.com/repos/%s/git/commits/%s" % (slug, commit), headers=headers)
     return res.json()['tree']['sha']
 
 
 def update_reference(slug, ref, commit):
-    print(slug + " \033[92mPatch reference\033[0m")
+    print("    \033[92mpatch reference\033[0m")
     curl('patch', "https://api.github.com/repos/%s/git/refs/heads/%s" % (slug, ref), headers=headers,
          data=dumps(dict(sha=commit)))
     return True
@@ -64,11 +60,10 @@ try:
     repos = ['codecov/example-java', 'codecov/example-scala', 'codecov/example-xcode', 'codecov/example-c',
              'codecov/example-lua', 'codecov/example-go', 'codecov/example-python', 'codecov/example-php',
              'codecov/example-d', 'codecov/example-fortran', 'codecov/example-swift']
-    total = len(repos)
 
     lang = os.getenv('TEST_LANG', 'python')
     slug = os.getenv('TEST_SLUG', 'codecov/codecov-'+lang)
-    sha = os.getenv('TEST_SHA', 'master')
+    sha = os.getenv('TEST_SHA', get_head('codecov/codecov-'+lang, 'master'))
     cmd = os.getenv('TEST_CMD', None)
     if not cmd:
         if lang == 'python':
@@ -82,13 +77,14 @@ try:
     # Make empty commit
     commits = {}
     for _slug in repos:
+        print(_slug)
         # set pending status
         set_state(slug, sha, "pending", _slug)
 
         # https://developer.github.com/v3/git/commits/#create-a-commit
         head = get_head(_slug, 'future')
         tree = get_tree(_slug, head)
-        print(_slug + " \033[92mPost commit\033[0m")
+        print("    \033[92mpost commit\033[0m")
         args = (os.getenv('CIRCLE_BUILD_NUM'), circleurl, cmd.replace(' --user', '') if 'python' in _slug else cmd)
         res = curl('post', "https://api.github.com/repos/%s/git/commits" % _slug,
                    headers=headers,
@@ -97,30 +93,31 @@ try:
                                    parents=[head],
                                    author=dict(name="Codecov Test Bot", email="hello@codecov.io"))))
         _sha = res.json()['sha']
-        print("    Sha: " + _sha)
+        print("    new commit: " + _sha)
         update_reference(_slug, 'future', _sha)
         commits[_slug] = _sha
 
     # wait for travis to pick up builds
-    print("Waiting 4 minutes...")
+    print("==================================================\nWaiting 4 minutes...\n==================================================")
     time.sleep(240)
 
     # Wait for CI Status
-    passed, total = 0, len(commits)
+    passed = 0
     while len(commits) > 0:
-        print("Waiting 1 minutes...")
+        print("====================================================\nWaiting 1 minute...\n====================================================")
         time.sleep(60)
         # collect build numbers
         for _slug, commit in commits.items():
             try:
                 res = curl('get', "https://api.github.com/repos/%s/commits/%s/status" % (_slug, commit), headers=headers).json()
                 state = res['state']
-                if state == 'pending':
-                    continue
-
                 print(_slug)
                 travis_target_url = res['statuses'][0]['target_url']
                 print('    \033[92mCI Status:\033[0m ' + state + ' @ ' + travis_target_url)
+
+                if state == 'pending':
+                    set_state(slug, sha, 'pending', _slug, url=travis_target_url)
+                    continue
 
                 # ASSERT status must be successful
                 assert state == 'success', "CI status %s" % state
@@ -150,9 +147,11 @@ try:
                     diff = unified_diff(dumps(master, indent=2, sort_keys=True).split('\n'),
                                         dumps(future, indent=2, sort_keys=True).split('\n'),
                                         fromfile='master', tofile='future')
+                    print("    \033[92mcreate gist\033[0m")
                     # https://developer.github.com/v3/gists/#edit-a-gist
                     res = curl('post', 'https://api.github.com/gists', headers=headers,
-                               data=dumps(dict(description=_slug, files={"diff.diff": {"content": "".join((diff.next(), diff.next(), diff.next(), "\n".join(diff)))}})))
+                               data=dumps(dict(description=_slug.replace('/', ' '),
+                                               files={"diff.diff": {"content": "".join((diff.next(), diff.next(), diff.next(), "\n".join(diff)))}})))
                     print("    Report Failed.")
                     set_state(slug, sha, 'failed', _slug, res.json()['html_url'], url=travis_target_url)
 
@@ -163,7 +162,7 @@ try:
                 print('    '+str(e))
                 del commits[_slug]
 
-    sys.exit(passed < total)
+    sys.exit(passed < len(repos))
 
 except Exception:
     [set_state(slug, sha, 'error', _slug, str(e)) for _slug in commits.keys()]
